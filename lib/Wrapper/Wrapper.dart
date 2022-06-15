719fb9e4c601c3d4dc4cd1b514d9740d7c5447d8
rodrigue3 <rojuvence2000@yahoo.de>
2022-06-15 11:45:09 +0200

    Firebase Verbindung

---
+++ lib/Wrapper/Wrapper.dart
@@ -0,0 +1,25 @@
+import 'package:broetchenshop/LoginScreen/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../MainScreen/Offer/mainoffer_screen.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot){
+          if(snapshot.hasData){
+            return const MainOfferScreen();
+          } else{
+            return const MainLoginScreen();
+          }
+        },
+      ),
+    );
+  }
+}
